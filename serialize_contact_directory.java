cd757b1215781c02570865843f3e0865d7c503d7
amandeepfull <amandeep.santokh@conversionsupport.com>
2017-04-17 15:56:24 +0530

    modified

--- serialize_contact_directory.java
+++ serialize_contact_directory.java
@@ -1,28 +1,28 @@
-	import java.io.*;
+import java.io.*;
 import java.util.*;
 class abc
 {
 	BufferedReader br1=new BufferedReader(new InputStreamReader(System.in));
-	HashMap<String,HashMap<String,String>> hss =new HashMap<String,HashMap<String,String>>(); 
+	HashMap<String,HashMap<String,String>> directory =new HashMap<String,HashMap<String,String>>(); 
+	FileOutputStream fos=new FileOutputStream("contacts.ser");
+		ObjectOutputStream oos=new ObjectOutputStream(fos);
+		FileInputStream fis=new FileInputStream("contacts.ser");
+		ObjectInputStream ois =new ObjectInputStream(fis);
 	public void add(String name)
 	{
 		try{
 		
-			HashMap<String,String> hm= new HashMap<String,String>();
+			HashMap<String,String> contacts= new HashMap<String,String>();
 		System.out.println("Enter the place for u Want to save the Contact:	");
 		String pla=br1.readLine();
 		System.out.println("Enter the Number for your Contact:	");
 		String num=br1.readLine();
-		hm.put(pla,num);
-		hss.put(name,hm);
-		FileOutputStream fos=new FileOutputStream("contacts.ser");
-		ObjectOutputStream oos=new ObjectOutputStream(fos);
-		oos.writeObject(hss);
+		contacts.put(pla,num);
+		directory.put(name,contacts);
+		
+		oos.writeObject(directory);
 			System.out.println("########################Contact entered Successfully ######################3");
 		}
-				
-	
-		
 		catch(Exception e)
 		{
 			
@@ -31,19 +31,18 @@ class abc
 	public void add_on_existing(String name)
 	{
 		try{	
-		FileInputStream fis=new FileInputStream("contacts.ser");
-		ObjectInputStream ois =new ObjectInputStream(fis);
-		HashMap<String,HashMap<String,String>> hss1=(HashMap)ois.readObject();
-		if(hss1.containsKey(name))
+		
+		HashMap<String,HashMap<String,String>> directory1=(HashMap)ois.readObject();
+		if(directory1.containsKey(name))
 		{
 			System.out.println("Enter the place for your Contact:	");
 			String pla=br1.readLine();
 			System.out.println("Enter the Number for your Place:	");
 			String num=br1.readLine();
-			hss1.get(name).put(pla,num);
+			directory1.get(name).put(pla,num);
 		FileOutputStream fos=new FileOutputStream("contacts.ser");
 		ObjectOutputStream oos=new ObjectOutputStream(fos);
-		oos.writeObject(hss1);
+		oos.writeObject(directory1);
 			System.out.println("#####################Contact Updated Successfully####################");	
 		}
 		else
@@ -58,12 +57,11 @@ class abc
 	public void search(String name)
 	{
 	try{
-		FileInputStream fis=new FileInputStream("contacts.ser");
-		ObjectInputStream ois =new ObjectInputStream(fis);
-		HashMap<String,HashMap<String,String>> hss1=(HashMap)ois.readObject();	
-		if(hss1.containsKey(name))
+		
+		HashMap<String,HashMap<String,String>> directory1=(HashMap)ois.readObject();	
+		if(directory1.containsKey(name))
 		{
-			System.out.println(hss1.get(name));
+			System.out.println(directory1.get(name));
 		}
 	else{
 		System.out.println("No such Contact Available");
@@ -77,10 +75,9 @@ class abc
 	{	
 		try{
 		System.out.println("Your full Contact Directory\n");
-		FileInputStream fis=new FileInputStream("contacts.ser");
-		ObjectInputStream ois =new ObjectInputStream(fis);
-		HashMap<String,HashMap<String,String>> hss1=(HashMap)ois.readObject();	
-		for(Map.Entry m: hss1.entrySet())
+		
+		HashMap<String,HashMap<String,String>> directory1=(HashMap)ois.readObject();	
+		for(Map.Entry m: directory1.entrySet())
 		{
 			System.out.println(m.getKey());
 		}
