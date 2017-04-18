362119c84cf7c3d8b3cbd6e31c6ce2e9d175e592
amandeepfull <amandeep.santokh@conversionsupport.com>
2017-04-18 16:57:53 +0530

    pojo

---
+++ pojo.java
@@ -0,0 +1,115 @@
+import java.io.*;
+import java.util.*;
+class Directory{
+	
+	String bussiness;
+	String work;
+	String home;
+	String Address;
+	public void putValue(String buss,String wo,String ho,String Add)
+	{
+		bussiness=buss;
+		work=wo;
+		home=ho;
+		Address=Add;
+	}
+	
+	public String getBusiness()
+	{
+		return bussiness;
+	}
+	public String getWork()
+	{
+		return work;
+	}
+	public String getHome()
+	{
+		return home;
+	}
+	public String getAddress()
+	{
+		return Address;
+	}
+	
+	public String toString()
+	{
+		System.out.println("######################Your Contact Details#####################");
+		
+		return "business="+bussiness+"\n"+"work="+work+"\n"+"home="+home+"\n"+"Address="+Address;
+	}
+}
+
+class Driver{
+public static void main(String agr[]) throws Exception
+{
+	
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	HashMap<String,Directory> contacts=new HashMap<String,Directory>();
+while(true)
+{
+	System.out.println("1. Add a contact\n2. Search \n3. Show full Directory\n4.Delete a Contact \n5.Exit\n" );
+System.out.println("Enter your choice:	");
+	
+	int n=Integer.parseInt(br.readLine());
+	switch (n)
+	{
+		case 1:
+					
+					System.out.println("Enter the name U want to save:	");
+					String name=br.readLine();
+					System.out.println("Enter the contact number for your bussiness place:	");
+					String bus=br.readLine();
+					System.out.println("Enter the contact number for your work place:	");
+					String wo=br.readLine();
+					System.out.println("Enter the contact number for your Home place:	");
+					String hom=br.readLine();
+					System.out.println("Enter Your Address	");
+					String ad=br.readLine();
+					Directory d=new Directory();
+					d.putValue(bus,wo,hom,ad);
+					contacts.put(name,d);	
+						break;
+		case 2: 
+					System.out.println("Enter the name U want to Search:	");
+					String s=br.readLine();
+					if(contacts.containsKey(s))
+					{
+						System.out.println(contacts.get(s));
+						
+						System.out.println("###############################");
+					}
+					else
+					{
+						System.out.println("Sorry no any contact Available!!!!!!!!!!!!!!!!!11");
+					}
+					
+					break;
+		case 3: 
+					System.out.println("###################Your full directory is as follows: #######################");
+					int i=1;
+					for(Map.Entry m:contacts.entrySet())
+					{
+						System.out.println(i+"."+m.getKey());
+						i++;
+					}
+					
+					System.out.println("#########################");
+						break;
+			
+		case 4: 
+					
+		break;
+		
+		
+		case 5: 
+				System.exit(0);
+		break;
+						
+		 default:	System.out.println("Enter the valid Choice:	");
+	
+	}
+
+	
+}
+}
+}
